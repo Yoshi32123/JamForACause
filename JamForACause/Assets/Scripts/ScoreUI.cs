95b10a9b9269ef0d00ea0b552a44b72c6d1964ad
crlimacastro <crlimacastro@gmail.com>
2019-11-15 22:14:52 -0500

    Created Score UI

--- JamForACause/Assets/Scripts/ScoreUI.cs
+++ JamForACause/Assets/Scripts/ScoreUI.cs
@@ -1,13 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class ScoreUI : MonoBehaviour
 {
+    [SerializeField] private int score;
+
+    private Text scoreText;
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            if (value >= 0)
+                score = value;
+
+            scoreText.text = "Score: " + score;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set starting score at 0
+        score = 0;
+
+        // Get score Text
+        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
