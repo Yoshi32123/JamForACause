ca872e6ccfa2daa54fbe93e1fd30909f55e9d56e
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-11-15 20:39:32 -0500

    Item will follow mouse if mouse is clicked down on that object until it is let go

--- JamForACause/Assets/Scripts/Item.cs
+++ JamForACause/Assets/Scripts/Item.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.5f;
+    public bool mouseHover = false;
+
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Hover();
+
+            if (mouseHover)
+            {
+                mousePosition = Input.mousePosition;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            }
+        }
+        else
+        {
+            mouseHover = false;
+        }
+    }
+
+    /// <summary>
+    /// Detects if mouse is hovering over an object with tag "Pickup"
+    /// </summary>
+    public void Hover()
+    {
+        Ray ray;
+        RaycastHit2D hit;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "Pickup")
+            {
+                //Debug.Log("object hit");
+                mouseHover = true;
+            }
+        }
     }
 }
