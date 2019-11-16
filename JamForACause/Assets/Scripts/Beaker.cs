dd25e83d8b2b79c10676ec22e0ccc6f73f27452c
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-11-15 22:53:06 -0500

    i didnt push in a bit so i am now

---
+++ JamForACause/Assets/Scripts/Beaker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beaker : MonoBehaviour
+{
+    [SerializeField] bool mouseHover = false;
+    [SerializeField] GameObject element = null;
+    private Vector3 mousePosition;
+    [SerializeField] GameObject mainCam;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+        gameManager = mainCam.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseHover && !gameManager.elementSelected)
+        {
+            mousePosition = Input.mousePosition;
+            Instantiate(element, Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+            mouseHover = false;
+            gameManager.elementSelected = true;
+        }
+    }
+
+    /// <summary>
+    /// Detects if mouse is hovering over an object with tag "Pickup"
+    /// </summary>
+    private void Hover()
+    {
+        Ray ray;
+        RaycastHit2D hit;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "Beaker")
+            {
+                if (hit.collider.name == gameObject.name)
+                {
+                    //Debug.Log(hit.collider.name);
+                    mouseHover = true;
+                }
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Hover();
+    }
+}
---
+++ JamForACause/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool elementSelected = false;
+
+    public GameObject dish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
