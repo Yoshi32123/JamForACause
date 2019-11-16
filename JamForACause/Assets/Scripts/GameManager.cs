4e86aceb84b4080da01e0aa9ba3b6dbbf85b8ce1
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-11-16 16:49:18 -0500

    Update GameManager.cs

--- JamForACause/Assets/Scripts/GameManager.cs
+++ JamForACause/Assets/Scripts/GameManager.cs
@@ -229,6 +229,10 @@ public class GameManager : MonoBehaviour
                         molecule = null;
                         index = i;
                     }
+                    else
+                    {
+                        // wrong item
+                    }
                 }
             }
 
