c49fe5dd811ffdf94034f4c984a35bcab8fb0420
crlimacastro <crlimacastro@gmail.com>
2019-11-15 20:22:53 -0500

    Finished Main Menu
    Only the instructions menu is left once the game is created

---
+++ JamForACause/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject instructionsMenu;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        instructionsMenu.SetActive(false);
+    }
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void ClickInstructions()
+    {
+        mainMenu.SetActive(false);
+        instructionsMenu.SetActive(true);
+    }
+
+    public void ClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void ClickBack()
+    {
+        mainMenu.SetActive(true);
+        instructionsMenu.SetActive(false);
+    }
+}
