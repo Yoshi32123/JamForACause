0b052730e8f5a9702a5b06a9f232ba358e0d20a4
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-11-15 21:31:07 -0500

    updates to scene

--- JamForACause/Assets/Scripts/Item.cs
+++ JamForACause/Assets/Scripts/Item.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     private Vector3 mousePosition;
-    public float moveSpeed = 0.5f;
-    public bool mouseHover = false;
+    [SerializeField] float moveSpeed = 0.5f;
+    [SerializeField] bool mouseHover = false;
 
     // Use this for initialization
     void Start()
@@ -25,7 +25,7 @@ public class Item : MonoBehaviour
             {
                 mousePosition = Input.mousePosition;
                 mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+                this.transform.position = Vector2.Lerp(this.transform.position, mousePosition, moveSpeed);
             }
         }
         else
@@ -37,21 +37,23 @@ public class Item : MonoBehaviour
     /// <summary>
     /// Detects if mouse is hovering over an object with tag "Pickup"
     /// </summary>
-    public void Hover()
+    private void Hover()
     {
         Ray ray;
         RaycastHit2D hit;
 
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
 
         if (hit.collider != null)
         {
             if (hit.collider.gameObject.tag == "Pickup")
             {
-                //Debug.Log("object hit");
-                mouseHover = true;
+                if (hit.collider.name == gameObject.name)
+                {
+                    Debug.Log(hit.collider.name);
+                    mouseHover = true;
+                }
             }
         }
     }
